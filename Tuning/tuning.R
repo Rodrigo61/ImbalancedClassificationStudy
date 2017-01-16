e5bf60ec8aff349531cf6371972f3e886e4af896
Rodrigo61 <rodrigoaf61@yahoo.com.br>
2017-01-16 15:11:40 -0200

    Alteracao script de acordo com rueniao 2

--- Tuning/tuning.R
+++ Tuning/tuning.R
@@ -9,6 +9,8 @@
 
 library(mlr)
 library(stringr)
+library(caTools)
+library(caret)
 set.seed(3)
 
 ################################################################
@@ -16,7 +18,7 @@ set.seed(3)
 ################################################################
 
 #Quantas iteracoes serao feitas no random search
-MAX_IT = 10L
+MAX_IT = 1L
 #parametro K do K-folds
 ITERS = 3L
 
@@ -38,86 +40,119 @@ dataset_dir = dirname(dataset_path)
 #Carregando dataset
 dataset = read.csv(dataset_path, header = T)
 
-################################################################
-####################  CONF DO CV  ##############################
-################################################################
-#Definindo o controle do search
-ctrl = makeTuneControlRandom(maxit = MAX_IT)
+#Definindo Data frame com resultados para saida
+out_df = NULL
 
-#Definindo método de CV
-rdesc = makeResampleDesc("CV", iters = ITERS)
 
-MAJORITY_WEIGHT = length(which(dataset['y_data'] == 1))/length(which(dataset['y_data'] == 0))
+################################################################
+####################  FUNCOES     ##############################
+################################################################
+get_measures_from_tuneParams = function(search_space, dataset, learner_str, measure, weight_space=F){
+  
+  #Definindo variáveis de controle
+  
+  if(weight_space == T){
+    MAJORITY_WEIGHT = length(which(dataset['y_data'] == 1))/length(which(dataset['y_data'] == 0))  
+  }else{
+    MAJORITY_WEIGHT = 1 #remove a influencia do cost learn
+  }
+  
+  TRAIN_PERCENT = 0.8
+  
+  #Definindo configuracoes pro CV(k_fold  )
+  ctrl = makeTuneControlRandom(maxit = MAX_IT)
+  rdesc = makeResampleDesc("CV", iters = ITERS)
+  
+  #Definindo variavel de retorno da funcao
+  result = NULL
+  
+  #Seperando treino e teste
+  train_index = createDataPartition(1:length(dataset[,'y_data']), 
+                                    times = 1,
+                                    p = TRAIN_PERCENT,
+                                    list = F)
+  train = dataset[train_index,]
+  test = dataset[-train_index,]
+  
+  #Realizando o tuning com a métrica escolhida
+  res_tuneParams = tuneParams(learner_str, task = makeClassifTask(data=train, target='y_data', positive='1'), resampling = rdesc,
+                           par.set = num_ps, control = ctrl, measure=measure, show.info = DEBUG)
+  result$performance_tuned = res_tuneParams$y
+  
+  #Treinando um modelo com o treino e os hiperparametros obtidos e armazenando a performance
+  learner = setHyperPars(makeLearner(learner_str), par.vals = res_tuneParams$x)
+  learner_res = mlr::train(learner, makeClassifTask(data=train, target='y_data', positive='1'))
+  p = predict(learner_res, task = makeClassifTask(data=test, target='y_data', positive='1'))
+  result$performance_trained = performance(p, measures = acc)
+  
+  return(result)
+}
+
+
+gen_all_measures_inline = function(search_space, dataset, learner_str, weight_space){
+  
+  measures_compilation = NULL
+  #Repetimos 3x a busca pelas performances
+  for (i in 1:ITERS){
+    #Realizando Tuning com métrica acurácia
+    res_acc = get_measures_from_tuneParams(search_space = num_ps, dataset = dataset, learner_str = learner_str, measure = acc, weight_space = weight_space)
+    #Realizando Tuning com métrica F1
+    res_f1 = get_measures_from_tuneParams(search_space = num_ps, dataset = dataset, learner_str = learner_str, measure = f1, weight_space = weight_space)
+    #Realizando Tuning com métrica gmean
+    res_gmean = get_measures_from_tuneParams(search_space = num_ps, dataset = dataset, learner_str = learner_str, measure = gmean, weight_space = weight_space)
+    
+    #sem weight space
+    new_row = c(learner_str, weight_space, 
+                res_acc$performance_tuned, 
+                res_f1$performance_tuned, 
+                res_gmean$performance_tuned,
+                res_acc$performance_trained, 
+                res_f1$performance_trained, 
+                res_gmean$performance_trained, i)
+    
+    measures_compilation = list(measures_compilation, new_row)
+    
+    if(DEBUG == T){
+      print(learner_str)
+      print("---")
+      print("acc")
+      print(res_acc$y)
+      print("---")
+      print("f1")
+      print(res_f1$y)
+      print("---")
+      print("gmean")
+      print(res_gmean$y)
+    }
+  }
+  return(measures_compilation)
+}
+
 ################################################################
 ####################  TUNING SVM  ##############################
 ################################################################
-
 #Definindo Search space
 num_ps = makeParamSet(
   makeNumericParam("C", lower = -10, upper = 10, trafo = function(x) 10^x),
   makeNumericParam("sigma", lower = -10, upper = 10, trafo = function(x) 10^x)
 )
 
-#Realizando Tuning com métrica acurácia
-res_acc_svm = tuneParams("classif.ksvm", task = makeClassifTask(data=dataset, target='y_data', positive='1'), resampling = rdesc,
-                     par.set = num_ps, control = ctrl, measure=acc, show.info = DEBUG)
+#Gerando performance para o cenário normal e weight de treinamento
+normal_learn = gen_all_measures_inline(search_space = num_ps, 
+                                       dataset = dataset,
+                                       learner_str = "classif.ksvm",
+                                       weight_space = F)
+weight_space_learn = gen_all_measures_inline(search_space = num_ps, 
+                                             dataset = dataset,
+                                             learner_str = "classif.ksvm",
+                                             weight_space = T)
 
-
-#Realizando Tuning com métrica F1
-res_f1_svm = tuneParams("classif.ksvm", task = makeClassifTask(data=dataset, target='y_data', positive='1'), resampling = rdesc,
-                    par.set = num_ps, control = ctrl, measure=f1, show.info = DEBUG)
-
-#Realizando Tuning com métrica gmean
-res_gmean_svm = tuneParams("classif.ksvm", task = makeClassifTask(data=dataset, target='y_data', positive='1'), resampling = rdesc,
-                       par.set = num_ps, control = ctrl, measure=gmean, show.info = DEBUG )
-
-if(DEBUG == T){
-  print("SVM")
-  print("---")
-  print("acc_svm")
-  print(res_acc_svm$y)
-  print("---")
-  print("f1_svm")
-  print(res_f1_svm$y)
-  print("---")
-  print("gmean_svm")
-  print(res_gmean_svm$y)
+#Armazenando resultados das perfomances no dataframe final 
+for(i in 1:ITERS){
+  out_df = rbind(out_df, normal_learn[[i]])
+  out_df = rbind(out_df, weight_space_learn[[i]])
 }
-################################################################
-####################  TUNING SVM  ##############################
-################################################################
 
-#Definindo Search space
-num_ps = makeParamSet(
-  makeNumericParam("C", lower = -10, upper = 10, trafo = function(x) 10^x),
-  makeNumericParam("sigma", lower = -10, upper = 10, trafo = function(x) 10^x)
-)
-
-#Realizando Tuning com métrica acurácia
-res_acc_svm_wg = tuneParams(makeWeightedClassesWrapper(makeLearner("classif.ksvm"), wcw.weight = MAJORITY_WEIGHT), task = makeClassifTask(data=dataset, target='y_data', positive='1'), resampling = rdesc,
-                         par.set = num_ps, control = ctrl, measure=acc, show.info = DEBUG)
-
-
-#Realizando Tuning com métrica F1
-res_f1_svm_wg = tuneParams(makeWeightedClassesWrapper(makeLearner("classif.ksvm"), wcw.weight = MAJORITY_WEIGHT), task = makeClassifTask(data=dataset, target='y_data', positive='1'), resampling = rdesc,
-                        par.set = num_ps, control = ctrl, measure=f1, show.info = DEBUG)
-
-#Realizando Tuning com métrica gmean
-res_gmean_svm_wg = tuneParams(makeWeightedClassesWrapper(makeLearner("classif.ksvm"), wcw.weight = MAJORITY_WEIGHT), task = makeClassifTask(data=dataset, target='y_data', positive='1'), resampling = rdesc,
-                           par.set = num_ps, control = ctrl, measure=gmean, show.info = DEBUG )
-
-if(DEBUG == T){
-  print("SVM WEIGHT")
-  print("---")
-  print("acc_svm")
-  print(res_acc_svm_wg$y)
-  print("---")
-  print("f1_svm")
-  print(res_f1_svm_wg$y)
-  print("---")
-  print("gmean_svm")
-  print(res_gmean_svm_wg$y)
-}
 ################################################################
 ####################  TUNING RF  ###############################
 ################################################################
@@ -127,85 +162,30 @@ num_ps = makeParamSet(
   makeDiscreteParam("mtry", c(1:(ncol(dataset)-1)))
 )
 
-#Realizando Tuning com métrica acurácia
-res_acc_rf = tuneParams("classif.randomForest", task = makeClassifTask(data=dataset, target='y_data', positive='1'), resampling = rdesc,
-                     par.set = num_ps, control = ctrl, measure=acc, show.info = DEBUG)
+#Gerando performance para o cenário normal e weight de treinamento
+normal_learn = gen_all_measures_inline(search_space = num_ps, 
+                                       dataset = dataset,
+                                       learner_str = "classif.randomForest",
+                                       weight_space = F)
+weight_space_learn = gen_all_measures_inline(search_space = num_ps, 
+                                             dataset = dataset,
+                                             learner_str = "classif.randomForest",
+                                             weight_space = T)
 
-
-#Realizando Tuning com métrica F1
-res_f1_rf = tuneParams("classif.randomForest", task = makeClassifTask(data=dataset, target='y_data', positive='1'), resampling = rdesc,
-                    par.set = num_ps, control = ctrl, measure=f1, show.info = DEBUG)
-
-#Realizando Tuning com métrica gmean
-res_gmean_rf = tuneParams("classif.randomForest", task = makeClassifTask(data=dataset, target='y_data', positive='1'), resampling = rdesc,
-                       par.set = num_ps, control = ctrl, measure=gmean, show.info = DEBUG )
-
-if(DEBUG == T){
-  print("RF")
-  print("---")
-  print("acc_RF")
-  print(res_acc_rf$y)
-  print("---")
-  print("f1_rf")
-  print(res_f1_rf$y)
-  print("---")
-  print("gmean_rf")
-  print(res_gmean_rf$y)
+#Armazenando resultados das perfomances no dataframe final 
+for(i in 1:ITERS){
+  out_df = rbind(out_df, normal_learn[[i]])
+  out_df = rbind(out_df, weight_space_learn[[i]])
 }
-
-
-################################################################
-####################  TUNING RF WEIGHT  ########################
-################################################################
-
-#Definindo Search space
-num_ps = makeParamSet(
-  makeDiscreteParam("mtry", c(1:(ncol(dataset)-1)))
-)
-
-#Realizando Tuning com métrica acurácia
-res_acc_rf_wg = tuneParams(makeWeightedClassesWrapper(makeLearner("classif.randomForest"), wcw.weight = MAJORITY_WEIGHT), task = makeClassifTask(data=dataset, target='y_data', positive='1'), resampling = rdesc,
-                        par.set = num_ps, control = ctrl, measure=acc, show.info = DEBUG)
-
-
-#Realizando Tuning com métrica F1
-res_f1_rf_wg = tuneParams(makeWeightedClassesWrapper(makeLearner("classif.randomForest"), wcw.weight = MAJORITY_WEIGHT), task = makeClassifTask(data=dataset, target='y_data', positive='1'), resampling = rdesc,
-                       par.set = num_ps, control = ctrl, measure=f1, show.info = DEBUG)
-
-#Realizando Tuning com métrica gmean
-res_gmean_rf_wg = tuneParams(makeWeightedClassesWrapper(makeLearner("classif.randomForest"), wcw.weight = MAJORITY_WEIGHT), task = makeClassifTask(data=dataset, target='y_data', positive='1'), resampling = rdesc,
-                          par.set = num_ps, control = ctrl, measure=gmean, show.info = DEBUG )
-
-if(DEBUG == T){
-  print("RF WEIGHT")
-  print("---")
-  print("acc_RF")
-  print(res_acc_rf_wg$y)
-  print("---")
-  print("f1_rf")
-  print(res_f1_rf_wg$y)
-  print("---")
-  print("gmean_rf")
-  print(res_gmean_rf_wg$y)
-}
-
 
 ################################################################
 ####################  GERANDO SAIDA  ###########################
 ################################################################
 
-svm_row = c(res_acc_svm$y, res_f1_svm$y, res_gmean_svm$y)
-svm_wg_row = c(res_acc_svm_wg$y, res_f1_svm_wg$y, res_gmean_svm_wg$y)
-rf_row = c(res_acc_rf$y, res_f1_rf$y, res_gmean_rf$y)
-rf_wg_row = c(res_acc_rf_wg$y, res_f1_rf_wg$y, res_gmean_rf_wg$y)
-
-out_df = NULL
-out_df = rbind(out_df, svm_row)
-out_df = rbind(out_df, svm_wg_row)
-out_df = rbind(out_df, rf_row)
-out_df = rbind(out_df, rf_wg_row)
-colnames(out_df) = c("accuracy", "f1", "gmeans")
-rownames(out_df) = c("SVM", "SVM_WG", "RF", "RF_WG")
-
+#Adicionando informacoes extras ao csv
+colnames(out_df) = c("learner", "weight_space", 
+                     "acc_tuned", "f1_tuned", 
+                     "gmeans_tuned", "acc_trained", 
+                     "f1_trained", "gmeans_trained", "time")
 out_path = paste(str_sub(dataset_path, start = 1, end = -5), "_summary.csv", sep="")
-write.table(out_df, out_path, col.names = T, row.names = T, sep=",")
+write.table(out_df, out_path, col.names = T, row.names = F, sep=",")
