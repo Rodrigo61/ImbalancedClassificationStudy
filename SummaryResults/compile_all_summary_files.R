af024a94a08d6fba337c5cb92361335c07340d6d
Rodrigo61 <rodrigoaf61@yahoo.com.br>
2017-02-02 12:21:52 -0200

    m

---
+++ SummaryResults/compile_all_summary_files.R
@@ -0,0 +1,29 @@
+SUMMARY_LIST_FILENAME = "summary_list"
+
+system("./generate_summary_list.sh")
+
+
+summary_list = read.csv(SUMMARY_LIST_FILENAME, header = F)
+
+df_final = NULL
+
+for(summary_file_name in summary_list){
+  
+  summary = reade.csv(summary_file_name, header = T)
+  summary_dir_path = dirname(summary_file_name)
+  dataset_imba_rate = str_extract(summary_dir_path, "0.[0-9]{2,3}")
+  dataset_name = basename(summary_dir_path)
+  
+  #Adicionando coluna 'dataset' e 'imba. rate'  
+  summary = cbind(summary, dataset_name)
+  names(summary)[ncol(summary)] = "dataset"
+  summary = cbind(summary, dataset_imba_rate)
+  names(summary)[ncol(summary)] = "imba. rate"
+
+  df_final = rbind(df_final, summary)
+}
+
+#Salvando dados do dataframe final
+out_filename = "summary_compilation.csv"
+write.table(df_final, out_filename, col.names = T, row.names = F, sep=",")
+print_debug(paste("Summary compilado e salvo em: ", out_filename, sep=""))
---
+++ SummaryResults/generate_summary_list.sh
@@ -0,0 +1,6 @@
+
+#LOCAL
+UCI_PATH="/home/rodrigo/Desktop/datasets_UCI/"
+SUMMARY_LIST_FILENAME="summary_list"
+
+find $UCI_PATH -name "*_summary*" > $SUMMARY_LIST_FILENAME
