c4dcfb57c2feffbe17e9bab90f5722aa090e132f
rodrigoaf <rodrigoaf@ssh.recod>
2017-03-25 15:26:47 -0300

    m

--- Tuning/individual_tuning/submission_files/gmeans_rf_false.sh
+++ Tuning/individual_tuning/submission_files/gmeans_rf_false.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-export PATH=/home/rodrigoaf/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export PATH=/home/rodrigoaf/R-3.2.5/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 Rscript --vanilla ../tuning.R --dataset_id=$@ --measure=gmeans --model=rf
