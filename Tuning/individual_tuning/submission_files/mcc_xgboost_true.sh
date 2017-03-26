3241c60c25ba25a7cfa4c381ae0f5a5b9cc673c0
rodrigoaf <rodrigoaf@ssh.recod>
2017-03-26 12:49:12 -0300

    m

--- Tuning/individual_tuning/submission_files/mcc_xgboost_true.sh
+++ Tuning/individual_tuning/submission_files/mcc_xgboost_true.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-export PATH=/home/rodrigoaf/R-3.2.5/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-Rscript --vanilla ../tuning.R --dataset_id=$@ --measure=mcc --model=xgboost --weight_space
+export PATH=/home/rodrigoaf/R-3.3.3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+Rscript --vanilla ../tuning.R --dataset_id=170 --measure=mcc --model=xgboost --weight_space
