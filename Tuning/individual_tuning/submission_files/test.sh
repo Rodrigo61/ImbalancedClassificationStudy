7a8a4ded8280ac5102c1bfa051b9542c042e7be1
rodrigoaf <rodrigoaf@ssh.recod>
2017-04-16 16:50:52 -0300

    m

--- Tuning/individual_tuning/submission_files/test.sh
+++ Tuning/individual_tuning/submission_files/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PATH=/home/rodrigoaf/R-3.3.3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-Rscript --vanilla ../tuning.R --dataset_id=0 --measure=f1 --model=xgboost --weight_space
+Rscript --vanilla ../tuning.R --dataset_id=0 --measure=f1 --model=xgboost --oversampling=adasyn --weight_space
