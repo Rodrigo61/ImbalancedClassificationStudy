2250d3fd34020cc9dfdc0e3665f29be2513ebd8f
rodrigoaf <rodrigoaf@ssh1.recod>
2017-06-06 19:44:55 -0300

    m

--- Tuning/individual_tuning/submission_files/gmeans_svm_borderline_1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export PATH=/home/rodrigoaf/R-3.3.3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-Rscript --vanilla ../tuning.R --dataset_id=$@ --measure=gmeans --model=svm --oversampling=borderline_1
