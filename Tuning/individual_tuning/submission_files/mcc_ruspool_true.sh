1c3ebdea33f6bbbf4d03c40ae09ee55b34a1f1e1
rodrigoaf <rodrigoaf@ssh1.recod>
2017-07-11 15:28:18 -0300

    m

--- Tuning/individual_tuning/submission_files/mcc_ruspool_true.sh
+++ Tuning/individual_tuning/submission_files/mcc_ruspool_true.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 export PATH=/home/rodrigoaf/R-3.3.3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-
-Rscript --vanilla ../tuning.R --dataset_id=23 --measure=acc --model=rusboost
+Rscript --vanilla ../tuning.R --dataset_id=$@ --measure=mcc --model=ruspool --weight_space
