0f9be6deb5f4300ad8f2b2e01473f58074d4a484
Rodrigo61 <rodrigoaf61@yahoo.com.br>
2017-08-12 13:58:08 -0300

    m

--- SummaryFixer/summary_fixer.R
+++ SummaryFixer/summary_fixer.R
@@ -9,7 +9,6 @@
 
 
 library(stringr)
-library(Jmisc)
 
 #**************************************************************#
 #*******************  FUNCOES   *******************************#
@@ -26,16 +25,9 @@ fix_missing_combination = function(summary){
   techniques$weight_space = c('TRUE')
   
   #Vamos fazer a busca para todas as combinacoes nesse summary
-  print("comeco das buscas")
   for (technique in names(techniques)) {
     
-    print("technique")
-    print(technique)
-    print("aaa")
     technique_options = techniques[[technique]]
-    print("BBB")
-    print("technique_options")
-    print(technique_options)
     for (option in technique_options){
       for(learner in learners){
         for(measure in measures){
@@ -43,29 +35,23 @@ fix_missing_combination = function(summary){
           combination_count = length(which(summary[, 'learner'] == learner 
                                            & summary[, 'measure'] == measure 
                                            & summary[, technique] == option))
-          
           # Nao existe medicao para essa combinacao, devemos gerar 3 linhas vazias entao
           if(combination_count == 0){
               print(paste("Combinacao faltante: leaner = ", learner, " measure = ", measure, " technique = ", technique, " option = ", option, sep =""))
 
-              print("a")
               empty_line = data.frame(learner, F, measure, F, F, NA, NA, NA, NA)
               names(empty_line) = names(summary)
-              print("b")
               
               summary = rbind(summary, empty_line)
               summary = rbind(summary, empty_line)
               summary = rbind(summary, empty_line)
-              print("c")
               
               # Inserimos todas as tecnicas como falsa e só atualizamos a tecnica da combinacao atual
               # isso só é possível pela suposicao de que as técnicas nao se misturam
-              summary[(NROW(summary)-3):NROW(summary), technique] = option
-              print("d")
-            
+              summary[(NROW(summary)-2):NROW(summary), technique] = option
             
           }else if(combination_count != 3){
-            warning(paste("A combinacao a seguir tem um número inesperado de medicoes = ", combination_count ," (!= 0 & != 3) [ ", "leaner = ", learner, " measure = ", measure, " technique = ", technique, " option = ", option, " ]", sep =""))
+            print(paste("A combinacao a seguir tem um número inesperado de medicoes = ", combination_count ," (!= 0 & != 3) [ ", "leaner = ", learner, " measure = ", measure, " technique = ", technique, " option = ", option, " ]", sep =""))
             stop()
           }
           
@@ -74,7 +60,6 @@ fix_missing_combination = function(summary){
     }
     
   }
-  print("fim das buscas")
   return(summary)
 }
 
@@ -93,13 +78,12 @@ for(summary_file_name in summary_list[,1]){
   summary_file_name = as.character(summary_file_name)
   summary = read.csv(summary_file_name, header = T)
   
-  print("1")
+  
   # Verifica todas as combinacoes que deveriam ter medicoes, mas nao tem e as completa com NA's
   summary = fix_missing_combination(summary)
-  print("2")
+
   #Ordenando pelas colunas o summary
   summary = summary[do.call(order, lapply(1:NCOL(summary), function(i) summary[, i])), ]
-  print("3")
   
   #Salvando summary atualizado
   write.table(summary, summary_file_name, col.names = T, row.names = F, sep=",")
